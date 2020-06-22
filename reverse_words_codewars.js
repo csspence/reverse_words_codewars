a891af2a1375f5977e9d2b082f5c9d6919dc9eef
Crew Spence <csspence123@gmail.com>
2020-06-22 07:26:39 -0400

    Wrote working solution.

--- reverse_words_codewars.js
+++ reverse_words_codewars.js
@@ -2,6 +2,15 @@
 Complete the solution so that it reverses all of the words within the string passed in.
 */
 
-function reverseWords(str){
-  return str; // reverse those words
+const reverseWords = (str) => {
+  let array = str.split(' ');
+  let newStr = '';
+  for(let i = array.length - 1; i > -1; i--) {
+    newStr += array[i];
+    if(i !== 0) {
+      newStr += ' ';
+    }
+  }
+
+  return newStr;
 }
